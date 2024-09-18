f11b11a652eaddcfd690ac2f9c4c62f16ce80b43
Antonella <antonella.beti@gmail.com>
2024-09-18 16:49:54 -0300

    se creo la estructura

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+//IMPORTACIONES
+import dotenv from "dotenv";
+
+dotenv.config();
+//CONFIGURACIÓN
+export default {
+    PORT: process.env.PORT || 4000,
+    DB_HOST: process.env.DB_HOST || "localhost",
+    DB_USER: process.env.DB_USER || "root",
+    DB_PASSWORD: process.env.DB_PASSWORD || "",
+    DB_NAME: process.env.DB_NAME || "prueba",
+    SECRET_KEY: process.env.SECRET_KEY || "mi_secreto_del_token",
+};
