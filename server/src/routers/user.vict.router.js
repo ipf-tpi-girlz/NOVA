0ec9d8fbf271ab32fc7649ab7430e2b45d41cec7
Antonella <antonella.beti@gmail.com>
2024-09-25 15:59:45 -0300

    se realizaron cambios

--- server/src/routers/user.vict.router.js
+++
@@ -1,9 +0,0 @@
-import { registerVict } from "../controllers/user.vict.controller.js";
-import { Router } from "express";
-
-export const routerVict = Router();
-
-//RUTA PARA REGISTRAR
-routerVict.post("/register", registerVict);
-//RUTA PARA LOGUEAR
-//routerVict.post("user/victima/login", loginVict);
