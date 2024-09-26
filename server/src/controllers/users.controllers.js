98761215ed0b578a589e2ff23994879b8b9c99af
Antonella <antonella.beti@gmail.com>
2024-09-25 21:21:51 -0300

    se borraron archivos no deseados

--- server/src/controllers/users.controllers.js
+++ server/src/controllers/users.controllers.js
@@ -1,13 +1,39 @@
 
 
 
-const regirterUser = (req, res) => {
+const regirterUser = async (req, res) => {
 
+    if (req.body.role === 'victima') {
+        const { } = req.body
+    }
+
+    if (req.body.role === 'profesional') {
+        const { } = req.body
+    }
+
+    if (req.body.role === 'institucion') {
+        const { } = req.body
+    }
 };
 
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
+    if (req.body.role === 'victima') {
+        const { } = req.body
+    }
+
+    if (req.body.role === 'profesional') {
+        const { } = req.body
+    }
+
+    if (req.body.role === 'institucion') {
+        const { } = req.body
+    }
 };
+
+const logout = async (req, res) => {
+
+}
 
 const deletUser = (req, res) => {
 
