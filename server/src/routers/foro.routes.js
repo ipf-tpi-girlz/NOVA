98243ab07123d9c09b8e578d3308e7b894494d7f
antpnella <antonella.beti@gmail.com>
2024-10-02 23:15:10 -0300

    correcciones

--- server/src/routers/foro.routes.js
+++ server/src/routers/foro.routes.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
-import { getForos, getInfoGeneral, createForo, deleteForo, updateForo } from "../controllers/foros.controller.js"
+import {
+  getForos,
+  getInfoGeneral,
+  createForo,
+  deleteForo,
+  updateForo,
+} from "../controllers/foros.controller.js";
 import { validarJWT } from "../middlewares/jwt.validation.js";
 
-
-export const foroRouter = Router()
+export const foroRouter = Router();
 //!trae foros y sub foros
-foroRouter.get('/', getForos);
+foroRouter.get("/foro", getForos);
 //!trae info general de los foros (comentarios y publis)
-foroRouter.get('/infoGeneral', getInfoGeneral);
+foroRouter.get("/infoGeneral", getInfoGeneral);
 //!crea foros
-foroRouter.post('/create', validarJWT, createForo);
+foroRouter.post("/create", validarJWT, createForo);
 //!edita foros
-foroRouter.put('/update/:id', updateForo);
+foroRouter.put("/update/:id", updateForo);
 //!elimina foros
-foroRouter.delete('/delete/:id', deleteForo);
+foroRouter.delete("/delete/:id", deleteForo);
