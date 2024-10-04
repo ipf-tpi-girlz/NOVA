c9992629da3de364e470a68347d1484e3317b27e
Antonella <antonella.beti@gmail.com>
2024-10-04 15:49:08 -0300

    se mergeo la rama anto con dev-sele

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -16,9 +16,8 @@ console.log(
 //MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
     //*Puertos del frontend permitidos
-    origin: ["http://127.0.0.1:5500",],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5173"],
     credentials: true
 }))
 app.use(session({
