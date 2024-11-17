330e5a2a704b9a402b30d74e73f5c15811945436
Maurcio Heredia <maurih46322945@gmail.com>
2024-11-17 13:48:25 -0300

    Users y Comunnity juntos

--- server/src/middlewares/jwt.validation.js
+++ server/src/middlewares/jwt.validation.js
@@ -1,35 +1,23 @@
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 import Usuario from "../models/users.js";
-import color from "chalk";
 const SECRET_KEY = config.SECRET_KEY;
 
-// Middleware para verificar el token JWT
 export const validarJWT = async (req, res, next) => {
   try {
-
-    // Obtener el token de las cookies o la sesión
     const token = req.cookies.authToken || req.session.token;
     console.log(token);
     if (!token) {
       return res.status(403).json({ message: "Token no proporcionado" });
     }
-
-    // Verificar el token
     const decoded = jwt.verify(token, SECRET_KEY);
-
-    // Buscar al usuario en la base de datos
     const user = await Usuario.findOne({ where: { id: decoded.userId } });
-
     if (!user) {
       return res
         .status(401)
         .json({ message: "Token inválido o usuario no encontrado", user });
     }
-
-    // Agregar el usuario a la request para acceder en el controlador
     req.user = user;
-
     next();
   } catch (error) {
     return res
--- server/src/routes/join.community.routes.js
+++ server/src/routes/join.community.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getUsersJoin, joinCommunity, deleteJoin } from "../controllers/join.community.controllers.js"
+import { getUsersJoin, joinCommunity, deleteJoin, getCommunitiesForUser } from "../controllers/join.community.controllers.js"
 import { validarJWT } from "../middlewares/jwt.validation.js";
 
 const routerJoinC = Router();
@@ -7,5 +7,6 @@ const routerJoinC = Router();
 routerJoinC.get("/:id", getUsersJoin)
 routerJoinC.post("/join/:id", validarJWT, joinCommunity)
 routerJoinC.delete("/delete/:id", validarJWT, deleteJoin)
+routerJoinC.get("/user/communities", validarJWT, getCommunitiesForUser);
 
 export default routerJoinC
